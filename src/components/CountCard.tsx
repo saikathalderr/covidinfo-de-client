3a5276718a8e3b1ab5c3c0b6de3f1c9c413166fa
saikathalder <saikathalderr@gmail.com>
2023-10-21 16:26:00 +0530

    feat: implemented germany info UI skeleton

--- src/components/CountCard.tsx
+++ src/components/CountCard.tsx
@@ -3,10 +3,25 @@ import clsx from "clsx";
 type CountCardProps = {
   title: string;
   count: number;
+  isLoading?: boolean;
   className?: string;
 };
 
-function CountCard({ title, count, className }: CountCardProps) {
+function CountCard({ title, count, isLoading, className }: CountCardProps) {
+  if (isLoading) {
+    return (
+      <div
+        className={clsx({
+          "animate-pulse py-10 flex flex-col items-center justify-center": true,
+          [`${className}`]: true,
+        })}
+      >
+        <p className="text-[.6rem] uppercase font-semibold">{title}</p>
+        <p className="animate-pulse bg-black/5 h-6 w-28"></p>
+      </div>
+    );
+  }
+
   return (
     <div
       className={clsx({
--- src/views/Germany.tsx
+++ src/views/Germany.tsx
@@ -4,6 +4,7 @@ import { useGermanyStore } from "@/store/useGermany";
 
 function Germany() {
   const fetchGermanyData = useGermanyStore((state) => state.fetchGermany);
+  const isLoading = useGermanyStore((state) => state.isLoading);
   const germanyTotalCases = useGermanyStore((state) => state.germany.cases);
   const germanyTotalDeaths = useGermanyStore((state) => state.germany.deaths);
   const germanyTotalRecovered = useGermanyStore(
@@ -23,21 +24,25 @@ function Germany() {
         <CountCard
           title="Cases"
           count={germanyTotalCases}
+          isLoading={isLoading}
           className="bg-blue-50/50 text-blue-700"
         />
         <CountCard
           title="Deaths"
           count={germanyTotalDeaths}
+          isLoading={isLoading}
           className="bg-red-50/50 text-red-700"
         />
         <CountCard
           title="Recovered"
           count={germanyTotalRecovered}
+          isLoading={isLoading}
           className="bg-green-50/50 text-green-700"
         />
         <CountCard
           title="Cases Per Week"
           count={germanyCasesPerWeek}
+          isLoading={isLoading}
           className="bg-amber-50/50 text-amber-700"
         />
       </div>
