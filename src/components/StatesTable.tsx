18f80212447f3e9b5ce3c5ac4c4793ce4e15fd0a
saikathalder <saikathalderr@gmail.com>
2023-10-22 13:47:22 +0530

    feat: added links in states table

--- src/components/StatesTable.tsx
+++ src/components/StatesTable.tsx
@@ -1,4 +1,5 @@
 import { GermanyState } from '@/interfaces/states.interface';
+import { Link } from 'react-router-dom';
 import { useEffect } from 'react';
 import { useGermanyStatesStore } from '@/store/useGermanyStates';
 
@@ -18,6 +19,7 @@ function StatesTable() {
             <table className="table text-xs font-mono w-full text-left">
                 <thead>
                     <tr className="bg-slate-100 text-slate-500 uppercase">
+                        <th className="px-2 py-1 border">#</th>
                         <th className="px-2 py-1 border">State</th>
                         <th className="px-2 py-1 border">Cases</th>
                         <th className="px-2 py-1 border">Deaths</th>
@@ -26,10 +28,13 @@ function StatesTable() {
 
                 <tbody>
                     {isLoading
-                        ? [1, 2, 3, 4, 5].map(() => {
+                        ? [...Array(16).keys()].map(() => {
                               return (
                                   <>
                                       <tr>
+                                          <td className="h-8 p-2 border">
+                                              <div className="bg-slate-50 border animate-pulse w-full h-full"></div>
+                                          </td>
                                           <td className="h-8 p-2 border">
                                               <div className="bg-slate-50 border animate-pulse w-full h-full"></div>
                                           </td>
@@ -43,13 +48,24 @@ function StatesTable() {
                                   </>
                               );
                           })
-                        : germanyAllStates.map((germanyState) => {
+                        : germanyAllStates.map((germanyState, idx) => {
                               const { id, name, cases, deaths } = germanyState;
+                              const stateLink = `/germany/states/${name}`;
+                              const stateCasesLink = stateLink + `/cases`;
+                              const stateDeathsLink = stateLink + `/deaths`;
+
                               return (
                                   <tr key={id}>
-                                      <td className="px-2 py-1 border">{name}</td>
-                                      <td className="px-2 py-1 border">{cases}</td>
-                                      <td className="px-2 py-1 border">{deaths}</td>
+                                      <td className="px-2 py-1 border">{idx + 1}</td>
+                                      <td className="px-2 py-1 border">
+                                          <Link to={stateLink}>{name}</Link>
+                                      </td>
+                                      <td className="px-2 py-1 border">
+                                          <Link to={stateCasesLink}>{cases}</Link>
+                                      </td>
+                                      <td className="px-2 py-1 border">
+                                          <Link to={stateDeathsLink}>{deaths}</Link>
+                                      </td>
                                   </tr>
                               );
                           })}
