c372c25187b642334de1b9ca6cb5526d045f2c14
saikathalder <saikathalderr@gmail.com>
2023-10-23 14:27:22 +0530

    feat: germany deaths sorting implemented

--- src/store/useDeaths.ts
+++ src/store/useDeaths.ts
@@ -1,4 +1,4 @@
-import { Death, FetchDeathsResponse } from '@/interfaces/deaths.interface';
+import { Death, DeathsOrder, DeathsSort, FetchDeathsResponse } from '@/interfaces/deaths.interface';
 
 import axios from 'axios';
 import { create } from 'zustand';
@@ -7,7 +7,7 @@ import { devtools } from 'zustand/middleware';
 interface DeathsState {
     deaths: Death[];
     isLoading: boolean;
-    fetchDeaths: () => void;
+    fetchDeaths: ({ sort, order }: { sort?: DeathsSort; order?: DeathsOrder }) => Promise<void>;
 }
 
 const __apiUrl = import.meta.env.VITE_API_ENDPOINT as string;
@@ -16,10 +16,12 @@ export const useDeathsStore = create<DeathsState>()(
     devtools((set) => ({
         deaths: [] as Death[],
         isLoading: false,
-        fetchDeaths: async () => {
+        fetchDeaths: async ({ sort, order }) => {
             set({ isLoading: true });
             const { data: fetchDeathsResp } = await axios.get<FetchDeathsResponse>(
-                `${__apiUrl}/germany/deaths`,
+                `${__apiUrl}/germany/deaths?sort=${sort || DeathsSort.DEATHS}&order=${
+                    order || DeathsOrder.DESC
+                }`,
             );
             const deaths = fetchDeathsResp.data;
             set({ deaths });
--- src/views/GermanyDeaths.tsx
+++ src/views/GermanyDeaths.tsx
@@ -1,3 +1,6 @@
+import { DeathsOrder, DeathsSort } from '@/interfaces/deaths.interface';
+import { useEffect, useState } from 'react';
+
 import Header from '@/components/common/Header';
 import { Icon } from '@iconify/react';
 import TBody from '@/components/common/TBody';
@@ -9,18 +12,64 @@ import Table from '@/components/common/Table';
 import TableSkeleton from '@/components/TableSkeleton';
 import dayjs from 'dayjs';
 import { useDeathsStore } from '@/store/useDeaths';
-import { useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
 
 function GermanyDeaths() {
+    const sectionName = 'Germany COVID Deaths';
+
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const [sort, setSort] = useState<DeathsSort | undefined>(undefined);
+    const [order, setOrder] = useState<DeathsOrder | undefined>(undefined);
+
     const fetchDeathsData = useDeathsStore((state) => state.fetchDeaths);
     const deaths = useDeathsStore((state) => state.deaths);
     const isLoading = useDeathsStore((state) => state.isLoading);
 
     useEffect(() => {
-        fetchDeathsData();
-    }, [fetchDeathsData]);
+        fetchDeathsData({ sort, order });
+    }, [fetchDeathsData, sort, order]);
 
-    const sectionName = 'Germany COVID Deaths';
+    useEffect(() => {
+        if (searchParams.get('order') === DeathsOrder.ASC) {
+            setOrder(DeathsOrder.ASC);
+        } else if (searchParams.get('order') === DeathsOrder.DESC) {
+            setOrder(DeathsOrder.DESC);
+        } else {
+            setOrder(undefined);
+        }
+    }, [searchParams]);
+
+    useEffect(() => {
+        if (searchParams.get('sort') === DeathsSort.DEATHS) {
+            setSort(DeathsSort.DEATHS);
+        } else if (searchParams.get('sort') === DeathsSort.DATE) {
+            setSort(DeathsSort.DATE);
+        } else {
+            setSort(undefined);
+        }
+    }, [searchParams]);
+
+    const toggleOrder = (sortField: DeathsSort) => {
+        let newOrder;
+        if (order === DeathsOrder.ASC) {
+            newOrder = DeathsOrder.DESC;
+        } else if (order === DeathsOrder.DESC) {
+            newOrder = DeathsOrder.ASC;
+        } else if (order === null) {
+            newOrder = DeathsOrder.DESC;
+        } else {
+            newOrder = DeathsOrder.DESC;
+        }
+        const newSort = sortField === DeathsSort.DEATHS ? DeathsSort.DEATHS : DeathsSort.DATE;
+        const params = new URLSearchParams(searchParams.toString());
+        params.set('order', newOrder);
+        params.set('sort', newSort);
+        setSearchParams(params);
+        setOrder(newOrder);
+        setSort(newSort);
+    };
+
     return (
         <>
             <Header title={sectionName} />
@@ -31,19 +80,47 @@ function GermanyDeaths() {
                             <THeader>#</THeader>
                             <THeader>
                                 Deaths
-                                <Icon icon="iconamoon:arrow-down-2-fill" width={20} />
+                                {order === DeathsOrder.DESC && sort === DeathsSort.DEATHS ? (
+                                    <Icon
+                                        icon="iconamoon:arrow-down-2-fill"
+                                        width={20}
+                                        className="cursor-pointer"
+                                        onClick={() => toggleOrder(DeathsSort.DEATHS)}
+                                    />
+                                ) : (
+                                    <Icon
+                                        icon="iconamoon:arrow-up-2-fill"
+                                        width={20}
+                                        className="cursor-pointer"
+                                        onClick={() => toggleOrder(DeathsSort.DEATHS)}
+                                    />
+                                )}
                             </THeader>
                             <THeader>
                                 Date
-                                <Icon icon="iconamoon:arrow-up-2-fill" width={20} />
+                                {order === DeathsOrder.DESC && sort === DeathsSort.DATE ? (
+                                    <Icon
+                                        icon="iconamoon:arrow-down-2-fill"
+                                        width={20}
+                                        className="cursor-pointer"
+                                        onClick={() => toggleOrder(DeathsSort.DATE)}
+                                    />
+                                ) : (
+                                    <Icon
+                                        icon="iconamoon:arrow-up-2-fill"
+                                        width={20}
+                                        className="cursor-pointer"
+                                        onClick={() => toggleOrder(DeathsSort.DATE)}
+                                    />
+                                )}
                             </THeader>
                         </TRow>
                     </THead>
 
                     <TBody>
                         {isLoading
-                            ? [...Array(16).keys()].map(() => {
-                                  return <TableSkeleton />;
+                            ? [...Array(16).keys()].map((idx) => {
+                                  return <TableSkeleton key={`skeleton-${idx + 1}`} />;
                               })
                             : deaths.map((deathChild, idx) => {
                                   const { deaths: deathsCount, date } = deathChild;
